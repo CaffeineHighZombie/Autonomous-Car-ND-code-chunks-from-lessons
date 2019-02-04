396eb08cfc78a876c6d25ae26a0481983123c991
RS Saranyaraj <saranyaraj@akitra.com>
2019-02-04 12:26:32 +0530

    plt.show() to get output out for all the rest of the code chunks Getting the combined threholds to produce output - Problem with threshold
    Changes to be committed:
    	modified:   8.3_Applying Sobel.py
    	modified:   8.4_Magnitude of the Gradient.py
    	modified:   8.5_Direction of the Gradient.py
    	modified:   8.6_Combining Thresholds.py

--- 8.6_Combining Thresholds.py
+++ 8.6_Combining Thresholds.py
@@ -10,9 +10,9 @@ def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # 2) Take the derivative in x or y given orient = 'x' or 'y'
     if orient=='x':
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
     else:
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     # 3) Take the absolute value of the derivative or gradient
     abs_sobel = np.absolute(sobel)
     # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
@@ -64,24 +64,23 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
 image = mpimg.imread('signs_vehicles_xygrad.png')
 
 # Choose a Sobel kernel size
-ksize = 3 # Choose a larger odd number to smooth gradient measurements
+ksize = 15 # Choose a larger odd number to smooth gradient measurements
 
 # Apply each of the thresholding functions
-gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
-grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
-mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
-dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
+grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(20, 100))
+mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(30, 100))
+dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0.7, 1.3))
 
-combined = mag_binary
-# combined = np.zeros_like(dir_binary)
-# combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+combined = np.zeros_like(dir_binary)
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
 
 # Plot the result
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
 ax1.imshow(image)
 ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(mag_binary, cmap='gray')
+ax2.imshow(combined, cmap='gray')
 ax2.set_title('Combined Image', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 plt.show()
